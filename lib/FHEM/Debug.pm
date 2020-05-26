ec80f5efa637ed708910f8019dacef6876591780
Christoph 'knurd' Morrison <git@christoph-jeschke.de>
2020-05-26 21:34:47 +0200

    Better use Exporter::Easy instead of Exporter

--- lib/FHEM/Debug.pm
+++ lib/FHEM/Debug.pm
@@ -3,12 +3,14 @@ package FHEM::Debug;
 
 use warnings;
 use strict;
-use Readonly;
 use Data::Dumper;
 use English qw{-no_match_vars};
-require Exporter;
-our @ISA = qw(Exporter);
-our @EXPORT_OK = qw(bark moan whisper);
+
+# non-core modules
+use Readonly;
+use Exporter::Easy (
+    OK => [q{bark}, q{moan}, q{whisper}]
+);
 
 
 Readonly my $MOAN_LOG_LEVEL =>  3;
@@ -17,20 +19,24 @@ Readonly my $BARK_LOG_LEVEL =>  1;
 sub bark {
     my $message = shift;
     ::Log($message, $BARK_LOG_LEVEL);
+    return;
 }
 
 sub moan {
     my $message = shift;
     ::Log($message, $MOAN_LOG_LEVEL);
+    return;
 }
 
 sub whisper {
     my $message = shift;
     ::Debug($message);
+    return;
 }
 
 sub write_log {
     my $message = shift;
+    return;
 }
 
 
